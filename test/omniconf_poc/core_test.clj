aa658972b1f4a57987753c3444e005855f658570
Aaron Peckham <apeckham@gmail.com>
2019-06-06 19:01:48 -0700

    use with-redefs cfg/config-values in tests

--- test/omniconf_poc/core_test.clj
+++ test/omniconf_poc/core_test.clj
@@ -2,19 +2,19 @@
   (:require [clojure.test :refer :all]
             [omniconf-poc.core :refer :all]
             [omniconf.core :as cfg]
-            [omniconf-poc.config :refer [define-cfg]]
             [omniconf-poc.commands :refer [connect disconnect]]))
 
-(defn setup-cfg [command values]
-  (define-cfg command)
+(defn setup-cfg [values]
   (cfg/populate-from-map values)
   (cfg/verify :silent true))
 
 (deftest connect-test
-  (setup-cfg :connect {:hostname "asdf"})
-  (is (= "Connecting to asdf!\n" (with-out-str (connect)))))
+  (with-redefs [cfg/config-values (atom (sorted-map))]
+    (setup-cfg {:command :connect :hostname "asdf"})
+    (is (= "Connecting to asdf!\n" (with-out-str (connect))))))
 
 (deftest disconnect-test
-  (setup-cfg :connect {:hostname "asdf"})
-  (cfg/set :confirm false)
-  (is (= "Disconnecting! (confirm is false)\n" (with-out-str (disconnect)))))
+  (with-redefs [cfg/config-values (atom (sorted-map))]
+    (setup-cfg {:command :disconnect :confirm true})
+    (cfg/set :confirm false)
+    (is (= "Disconnecting! (confirm is false)\n" (with-out-str (disconnect))))))
