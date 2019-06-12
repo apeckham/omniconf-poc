005ca2f2fd3ee71ca446616ae3ccc9673dbaa11c
Aaron Peckham <apeckham@gmail.com>
2019-06-11 19:30:17 -0700

    extract "when-command-is" helper

--- src/omniconf_poc/core.clj
+++ src/omniconf_poc/core.clj
@@ -8,10 +8,13 @@
     (when-not (re-matches regex value)
       (throw (ex-info (format "%s doesn't match %s" key (pr-str regex)) {})))))
 
+(defn when-command-is [& values]
+  #((set values) (cfg/get :command)))
+
 (cfg/define
   {:hostname {:description "where service is deployed"
               :type :string
-              :required #(#{:connect} (cfg/get :command))
+              :required (when-command-is :connect)
               :verifier (verify-regex #"[a-z-]+\.example\.com")}
    :port     {:description "HTTP port"
               :type :number
@@ -19,7 +22,7 @@
    :command {:type :keyword
              :one-of [:connect :disconnect]}
    :confirm {:type :boolean
-             :required #(#{:disconnect} (cfg/get :command))}})
+             :required (when-command-is :disconnect)}})
 
 (defn -main
   [& args]
